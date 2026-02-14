02ca73ca55274dc91a2f2ed35849a1520b4d204a
Mchack0101 <147649471+Mchack0101@users.noreply.github.com>
2026-02-14 23:40:53 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,13 @@ int main()
     while (1)
     {
         vglStartRendering();
-
         glClearColor(0.07f, 0.07f, 0.07f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
         vglEndRendering();
         vglSwapBuffers(GL_FALSE);
 
         SceCtrlData pad;
         sceCtrlPeekBufferPositive(0, &pad, 1);
-
         if (pad.buttons & SCE_CTRL_START)
             break;
     }
