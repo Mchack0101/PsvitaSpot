4f9faaa424e7241ef5c773dfbeff96addb189a4f
Mcishim <147649471+Mchack0101@users.noreply.github.com>
2026-02-23 00:12:07 +0000

    Update Application.h

--- include/Application.h
+++ include/Application.h
@@ -1,35 +1,24 @@
 #pragma once
 
-#include "Renderer.h"
-#include "InputHandler.h"
-#include "SpotifyAPI.h"
-#include "SpotifyUI.h"
-#include "PlaybackManager.h"
-#include <memory>
-
-namespace PsvitaSpot {
+class Renderer;
+class InputHandler;
 
 class Application {
+private:
+    Renderer* renderer;
+    InputHandler* inputHandler;
+    bool running;
+
 public:
     Application();
     ~Application();
-
-    bool initialize();
-    void run();
-    void shutdown();
-
+    
+    bool Initialize();
+    void Run();
+    void Shutdown();
+    bool IsRunning() const;
+    
 private:
-    void update();
-    void render();
-    bool initializeSpotify();
-
-    std::unique_ptr<Renderer> m_renderer;
-    std::unique_ptr<InputHandler> m_input;
-    std::shared_ptr<SpotifyAPI> m_spotifyAPI;
-    std::unique_ptr<SpotifyUI> m_spotifyUI;
-    std::unique_ptr<PlaybackManager> m_playbackManager;
-    bool m_running = false;
-    bool m_initialized = false;
+    void Update();
+    void Render();
 };
-
-}
