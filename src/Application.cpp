5e9481b8bb3ba825af2d5faab24d9f8790b12f9b
Mcishim <147649471+Mchack0101@users.noreply.github.com>
2026-02-23 00:15:13 +0000

    Update Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,123 +1,59 @@
 #include "Application.h"
-#include "Constants.h"
+#include "Renderer.h"
+#include "InputHandler.h"
 
-namespace PsvitaSpot {
-
-Application::Application()
-    : m_renderer(std::make_unique<Renderer>())
-    , m_input(std::make_unique<InputHandler>())
-    , m_spotifyAPI(std::make_shared<SpotifyAPI>())
-    , m_spotifyUI(std::make_unique<SpotifyUI>())
-    , m_playbackManager(std::make_unique<PlaybackManager>())
-    , m_running(false)
-    , m_initialized(false) {
-}
+Application::Application() : renderer(nullptr), inputHandler(nullptr), running(true) {}
 
 Application::~Application() {
-    shutdown();
+    Shutdown();
 }
 
-bool Application::initialize() {
-    if (m_initialized) {
-        return true;
-    }
-
-    if (!m_renderer->initialize()) {
+bool Application::Initialize() {
+    renderer = new Renderer();
+    if (!renderer->Initialize()) {
         return false;
     }
-
-    if (!m_spotifyUI->initialize()) {
-        return false;
-    }
-
-    initializeSpotify();
-
-    m_initialized = true;
+    
+    inputHandler = new InputHandler();
+    inputHandler->Initialize();
+    
     return true;
 }
 
-bool Application::initializeSpotify() {
-    std::string clientId = "";
-    std::string clientSecret = "";
-    std::string refreshToken = "";
-
-    if (clientId.empty() || clientSecret.empty() || refreshToken.empty()) {
-        return false;
-    }
-
-    if (!m_spotifyAPI->initialize(clientId, clientSecret, refreshToken)) {
-        return false;
-    }
-
-    m_playbackManager->initialize(m_spotifyAPI);
-    return true;
-}
-
-void Application::run() {
-    if (!m_initialized) {
-        return;
-    }
-
-    m_running = true;
-
-    while (m_running) {
-        update();
-        render();
+void Application::Run() {
+    while (running && IsRunning()) {
+        Update();
+        Render();
     }
 }
 
-void Application::shutdown() {
-    if (!m_initialized) {
-        return;
-    }
-
-    m_running = false;
-
-    if (m_spotifyUI) {
-        m_spotifyUI->shutdown();
-    }
-
-    if (m_renderer) {
-        m_renderer->shutdown();
-    }
-
-    m_initialized = false;
-}
-
-void Application::update() {
-    m_input->update();
-
-    if (m_input->shouldExit()) {
-        m_running = false;
-        return;
-    }
-
-    if (m_playbackManager) {
-        m_input->handleSpotifyControls(m_playbackManager.get());
-        m_playbackManager->update();
+void Application::Update() {
+    inputHandler->Update();
+    
+    if (inputHandler->IsButtonPressed(SCE_CTRL_START)) {
+        running = false;
     }
 }
 
-void Application::render() {
-    m_renderer->beginFrame();
-
-    m_renderer->clear(
-        Config::CLEAR_COLOR_R,
-        Config::CLEAR_COLOR_G,
-        Config::CLEAR_COLOR_B,
-        Config::CLEAR_COLOR_A
-    );
-
-    if (m_spotifyUI) {
-        bool connected = m_playbackManager && m_playbackManager->isConnected();
-        const PlaybackState& state = m_playbackManager
-            ? m_playbackManager->getCurrentState()
-            : PlaybackState{};
-
-        m_spotifyUI->render(state, connected);
-    }
-
-    m_renderer->endFrame();
+void Application::Render() {
+    renderer->Clear();
+    renderer->Present();
 }
 
+void Application::Shutdown() {
+    if (inputHandler) {
+        inputHandler->Shutdown();
+        delete inputHandler;
+        inputHandler = nullptr;
+    }
+    
+    if (renderer) {
+        renderer->Shutdown();
+        delete renderer;
+        renderer = nullptr;
+    }
+}
+
+bool Application::IsRunning() const {
+    return running;
 }
