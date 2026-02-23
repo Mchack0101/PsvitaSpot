1d7f34a211ca38aa7574d784595635e0fc38c48f
Mcishim <147649471+Mchack0101@users.noreply.github.com>
2026-02-23 01:47:13 +0000

    Update Renderer.cpp

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,25 +1,19 @@
 #include "Renderer.h"
-#include "Constants.h"
-#include <vitaGL.h>
-#include <psp2/display.h>
 
 Renderer::Renderer() {}
-
-Renderer::~Renderer() {
-    Shutdown();
-}
+Renderer::~Renderer() { Shutdown(); }
 
 bool Renderer::Initialize() {
-    vglInitExtended(0, Constants::VRAM_WIDTH, Constants::VRAM_HEIGHT, 0x60000, SCE_GXM_MULTISAMPLE_NONE);
-    
+    vglInitExtended(0, Constants::VRAM_WIDTH, Constants::VRAM_HEIGHT,
+        0x60000, SCE_GXM_MULTISAMPLE_NONE);
+
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glViewport(0, 0, Constants::DISPLAY_WIDTH, Constants::DISPLAY_HEIGHT);
-    
     return true;
 }
 
 void Renderer::Clear() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void Renderer::Present() {
@@ -27,13 +21,5 @@ void Renderer::Present() {
 }
 
 void Renderer::Shutdown() {
-    vglEnd();
-}
-
-float Renderer::GetDisplayWidth() const {
-    return Constants::DISPLAY_WIDTH;
-}
-
-float Renderer::GetDisplayHeight() const {
-    return Constants::DISPLAY_HEIGHT;
+    vglDeinit();
 }
